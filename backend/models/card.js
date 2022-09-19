52b075ea636c1342a8a795665215eb8a9415c9c6
Tatiana Berezhetskaya <t.berezhetskaya@gmail.com>
2022-09-19 19:11:30 +1000

    hotfixes after review

--- backend/models/card.js
+++ backend/models/card.js
@@ -12,7 +12,7 @@ const cardSchema = new mongoose.Schema({
     type: String,
     required: [true, 'Добавьте ссылку на фото'],
     validate: {
-      validator: (link) => validator.isUrl(link),
+      validator: (link) => validator.isURL(link),
       message: 'Укажите ссылку на фото',
     },
   },
